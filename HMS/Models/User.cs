9f2e067593745aced9aa3c7fb0f6480e8a732f78
Sofiia Yefimova <yefimova.sofiia4@gmail.com>
2026-01-02 15:42:14 +0200

    Add project files.

---
+++ HMS/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HMS.Models
+{
+	public class User : IdentityUser
+	{
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		
+	}
+
+}
